b36ddeeb6cd1894a91ad9709f2e9a808619e66e5
Talal Malik <mtm_531@hotmail.com>
2019-12-23 18:08:20 +0500

    setup react-native with redux and react-navigation@4

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
   Text, View
 } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from 'react-redux'
+
+import Store from './src/store/store';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen!!</Text>
+      </View>
+    );
+  }
+}
+
 
 const App = () => {
   return (
-    <View  style={{flex: 1,alignItems:'center', justifyContent: 'center'}}>
-      <Text>Welcome!</Text>
-    </View>
+    <Provider store={Store}>
+      <AppContainer />
+    </Provider>
   );
 };
+
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
 
 export default App;
